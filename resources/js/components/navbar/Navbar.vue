33b85135dff01a21f2479f492182b4a6c578503f
Eden <edenlim123@gmail.com>
2026-05-14 13:38:56 +0200

    Init: Nav component

---
+++ resources/js/components/navbar/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white w-full h-16"></div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/layout/Root.vue
+++ resources/js/layout/Root.vue
@@ -1,27 +1,30 @@
 <script setup>
 import Card from '../components/card/Card.vue'; // Import your existing Rating component
-
+import Navbar from "../components/navbar/Navbar.vue";
 </script>
 
 <template>
-    <div class="flex justify-space-between gap-[2rem]">
-        <Card title="Test.pdf" file-type=".pdf" />
+    <Navbar/>
+    <div class="p-4">
+        <div class="flex justify-space-between gap-[2rem]">
+            <Card title="Test.pdf" file-type=".pdf" />
 
-        <Card
-            title="IT-woche1.txt"
-            file-type=".txt"
-            :tags="['mathe', 'informatik']"
-            no-of-dislikes="23"
-            no-of-likes="39"
-        />
+            <Card
+                title="IT-woche1.txt"
+                file-type=".txt"
+                :tags="['mathe', 'informatik']"
+                no-of-dislikes="23"
+                no-of-likes="39"
+            />
 
-        <Card
-            title="Business-IT.txt"
-            file-type=".txt"
-            :tags="['wirtschaft', 'informatik']"
-            no-of-dislikes="3"
-            no-of-likes="369"
-        />
+            <Card
+                title="Business-IT.txt"
+                file-type=".txt"
+                :tags="['wirtschaft', 'informatik']"
+                no-of-dislikes="3"
+                no-of-likes="369"
+            />
+        </div>
     </div>
 </template>
 
