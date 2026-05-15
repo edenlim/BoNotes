5b35626b6123bb4966dcb37ea04d0ba1f91b0543
Eden <edenlim123@gmail.com>
2026-05-15 10:04:46 +0200

    Upgrade: Filter toggling Logic

--- resources/js/templates/Filter.vue
+++ resources/js/templates/Filter.vue
@@ -1,28 +1,21 @@
 <script setup>
-import {ref} from 'vue';
 const props = defineProps({
-    label: {type: String, default: null}
+    label: { type: String, default: null },
+    isActive: { type: Boolean, default: false } // Controlled by parent
 })
 
-const active = ref(false);
-
-const toggle = () => active.value = !active.value;
-
+const emit = defineEmits(['toggle'])
 </script>
 
 <template>
     <div
         class="text-center min-w-15 p-2 select-none border-2 rounded-xl cursor-pointer transition-all duration-200 hover:scale-105"
         :class="{
-            'border-secondary-text text-secondary-text' : !active,
-            'border-tertiary-text text-tertiary-text bg-primary ' : active
+            'border-secondary-text text-secondary-text' : !isActive,
+            'border-tertiary-text text-tertiary-text bg-primary ' : isActive
         }"
-        @click="toggle"
+        @click="emit('toggle')"
     >
-        {{label}}
+        {{ label }}
     </div>
 </template>
-
-<style scoped>
-
-</style>
