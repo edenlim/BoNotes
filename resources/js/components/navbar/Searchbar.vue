132d86d836dacf79f2a14047613b911a8a03e11e
Eden <edenlim123@gmail.com>
2026-05-18 22:08:26 +0200

    Update: Searchbar

--- resources/js/components/navbar/Searchbar.vue
+++ resources/js/components/navbar/Searchbar.vue
@@ -1,14 +1,14 @@
 <script setup>
-
+const searchQuery = defineModel('searchQuery', { type: String, default: '' });
 </script>
 
 <template>
 <div role="search" class="flex items-center">
-    <label for="searchbar" class="sr-only"></label>
+    <label for="searchbar" class="sr-only">Notizen Filterieren</label>
     <input
         id="live-filter"
         type="search"
-        v-model="query"
+        v-model="searchQuery"
         class="w-full p-3 transition-transform duration-200 border-2 rounded-xl focus:ring-primary bg-secondary"
         placeholder="Titel der Notizen..."
         aria-label="Filter notes"
