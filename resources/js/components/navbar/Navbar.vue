beb31fdd8feb7c8a29d8210f76739044cfad7eef
elim <eden@pottkinder.de>
2026-05-19 14:58:09 +0200

    Styling: More styling changes for Navbar

--- resources/js/components/navbar/Navbar.vue
+++ resources/js/components/navbar/Navbar.vue
@@ -9,18 +9,19 @@ const searchQuery = defineModel('searchQuery', { type: String, default: '' });
 
 <template>
     <nav class="
-        grid grid-cols-12 bg-white w-full items-center  py-2 gap-4 shadow-sm
+        grid grid-cols-12 bg-white w-full items-center py-2 shadow-sm
         px-2 md:px-8
+        gap-2 md:gap-4
     ">
         <img
             class="
-                w-auto max-w-none col-start-1 col-span-1 select-none hover-pop
+                w-auto max-w-none  col-span-1 select-none hover-pop
                 h-12 md:h-14 lg:h-16
             "
             :src="'/resources/images/bonotes.svg'"
             alt="BoNotes Logo"
         />
-        <Searchbar class="col-span-7 md:col-span-8 lg:col-span-4" v-model:searchQuery="searchQuery"/>
+        <Searchbar class="col-start-3 md:col-start-2 col-span-6 md:col-span-8 lg:col-span-4" v-model:searchQuery="searchQuery"/>
         <Filters class="col-span-2 md:col-span-1 lg:col-span-6" v-model:active-filter="activeFilter"/>
         <Account class="col-span-2 md:col-span-2 lg:col-span-1"/>
     </nav>
