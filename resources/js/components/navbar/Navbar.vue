e88346414947ab0b7d26f70182ab52acd440c699
Eden <edenlim123@gmail.com>
2026-05-23 11:48:15 +0200

    Styling: Navbar

--- resources/js/components/navbar/Navbar.vue
+++ resources/js/components/navbar/Navbar.vue
@@ -9,21 +9,25 @@ const searchQuery = defineModel('searchQuery', { type: String, default: '' });
 
 <template>
     <nav class="
-        grid grid-cols-12 bg-white w-full items-center py-2 shadow-sm sticky top-0 z-48
+        flex items-center justify-between bg-white w-full py-2 shadow-sm sticky top-0 z-48
         px-2 md:px-8
         gap-2 md:gap-4
     ">
         <img
             class="
-                w-auto max-w-none  col-span-1 select-none hover-pop
+                w-auto max-w-none select-none hover-pop flex-shrink-0
                 h-12 md:h-14 lg:h-16
             "
             :src="'/resources/images/bonotes.svg'"
             alt="BoNotes Logo"
         />
-        <Searchbar class="col-start-3 md:col-start-2 col-span-6 md:col-span-8 lg:col-span-4" v-model:searchQuery="searchQuery"/>
-        <Filters class="col-span-2 md:col-span-1 lg:col-span-6" v-model:active-filter="activeFilter"/>
-        <Account class="col-span-2 md:col-span-2 lg:col-span-1"/>
+
+        <Searchbar class="flex-grow max-w-xl mx-2 md:mx-4" v-model:searchQuery="searchQuery"/>
+
+        <div class="flex items-center gap-2 md:gap-4 flex-shrink-0">
+            <Filters v-model:active-filter="activeFilter"/>
+            <Account />
+        </div>
     </nav>
 </template>
 
