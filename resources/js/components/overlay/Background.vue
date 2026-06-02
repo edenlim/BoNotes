9da7af3b27dc4b30b1a230f27cec1172912e75f6
elim <eden@pottkinder.de>
2026-06-02 14:22:19 +0200

    Refactor: Remove style

--- resources/js/components/overlay/Background.vue
+++ resources/js/components/overlay/Background.vue
@@ -20,7 +20,14 @@ onBeforeUnmount(() => {
 
 <template>
     <Teleport to="body">
-        <Transition name="fade">
+        <Transition
+            enter-active-class="transition-opacity duration-300 ease-in-out"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="transition-opacity duration-300 ease-in-out"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
             <div
                 v-if="props.show"
                 class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm p-4"
@@ -38,6 +45,4 @@ onBeforeUnmount(() => {
 </template>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active { transition: opacity 0.3s ease; }
-.fade-enter-from, .fade-leave-to { opacity: 0; }
 </style>
